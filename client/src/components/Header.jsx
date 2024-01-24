ce76981db2f07b00340dcfc164df7568c173ae62
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-24 08:09:49 +0300

    Added Logo and search feature

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Navbar, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
-}
+    <Navbar className='border-b-2'>
+        <Link to="/" className='self-center whitespace-nowrap text-sm 
+        sm:text-xl font-semibold dark:text-white'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+            via-purple-500 to-pink-500 rounded-lg text-white'>safe</span>
+            Space
+        </Link>
+        <form>
+            <TextInput 
+                type='text'
+                placeholder='Search...'
+                rightIcon={AiOutlineSearch}
+                />
+        </form>
+    </Navbar>
+  );
+};
