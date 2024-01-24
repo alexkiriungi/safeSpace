0ddbec05b952d0fea0baf441e77a5935e7283179
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-24 12:38:42 +0300

    Database conection successful

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+
+
+const port = process.env.PORT || 3001;
+
+mongoose.connect(process.env.MONGO_URL).then(
+    () => {
+        console.log('Database connection successful');
+    }
+).catch((error) => {
+    console.log(error);
+});
 
 app.listen(port, ()=> {
     console.log(`Server is running on ${port}`);
