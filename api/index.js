864ef72cfd6e591e85a13a44526a20835c771097
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-24 22:38:37 +0300

    created an error handler

--- api/index.js
+++ api/index.js
@@ -26,3 +26,13 @@ app.listen(port, ()=> {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        sucess: false,
+        statusCode,
+        message
+    });
+});
