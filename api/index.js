35cb1509df94ec7084cbdcb1ed202ff9818ac1f0
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-24 21:04:17 +0300

    API sign up route functional

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.PORT || 3001;
 
@@ -21,4 +24,5 @@ app.listen(port, ()=> {
     console.log(`Server is running on ${port}`);
 });
 
-app.use("/api/user", userRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
