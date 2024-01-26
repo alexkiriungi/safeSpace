d7ae774143f2333df3486c5a7e49aa5167b7a6a6
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-26 15:57:22 +0300

    Added skeleton of the Dashboard sidebadr and profile info

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [ tab, setTab ] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search])
   return (
-    <div>Dashboard</div>
+    <div>
+      <div className="">
+        {/* sidebar */}
+        <DashSidebar />
+      </div>
+      <div className="">
+        {/* Right Side Profile info */}
+        {tab === 'profile' && <DashProfile />}
+      </div>
+    </div>
   )
 }
