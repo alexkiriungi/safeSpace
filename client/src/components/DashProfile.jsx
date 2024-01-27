38dff1ebcf4790f2f8a58d94fb246e766c381806
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-27 18:08:19 +0300

    minor changes

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -59,7 +59,7 @@ export default function DashProfile() {
                 getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                     setImageFileUrl(downloadURL);
                 });
-            }
+            },
         );
     };
 
