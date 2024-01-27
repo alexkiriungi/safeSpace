7b1951b50a5702c0fe14b63340cd1ae1c76eda33
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-27 11:22:57 +0300

    Completed User profile image image functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -18,7 +18,13 @@ export default function DashSidebar() {
         <Sidebar.Items>
             <Sidebar.ItemGroup>
                 <Link to='/dashboard?tab=profile'>
-                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label="user" labelColor='dark' >
+                    <Sidebar.Item 
+                    active={tab === 'profile'} 
+                    icon={HiUser} 
+                    label="user" 
+                    labelColor='dark'
+                    as='div' 
+                    >
                         Profile
                     </Sidebar.Item>
                 </Link>
