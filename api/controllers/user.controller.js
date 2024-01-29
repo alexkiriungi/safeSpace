d11d4a00c34d5d1ea50f469cc932c7562eecbd02
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-29 13:22:52 +0300

    Added the Signout API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -58,3 +58,11 @@ export const deleteUser = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signout = (req, res, next) => {
+    try {
+        res.clearCookie('access_token').status(200).json('User signed out successfully');
+    } catch (error) {
+        next(error);
+    }
+};
