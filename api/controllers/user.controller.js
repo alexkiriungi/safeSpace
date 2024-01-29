a077007d46a5aa0b2449398477839ae76a7c1f86
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-29 12:38:50 +0300

    delete account API route created

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -52,8 +52,9 @@ export const deleteUser = async (req, res, next) => {
         return next(errorHandler(403, "You are not allowed to delete this user"))
     }
     try {
-        
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User deleted');
     } catch (error) {
-        next(errror)
+        next(error)
     }
 };
