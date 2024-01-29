d2f88b336a84541d9d47694e4a93a6f8838570dc
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-29 09:48:38 +0300

    Completed the update profile route

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3001;
 
