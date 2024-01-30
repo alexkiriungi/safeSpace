8f3d75f2befd3b60fb0b264283e0267d4c95bd91
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-30 10:47:11 +0300

    Added the admin dashposts section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 
 
 export default function Dashboard() {
@@ -23,6 +24,8 @@ export default function Dashboard() {
   
         {/* Right Side Profile info */}
         {tab === 'profile' && <DashProfile />}
+        {/* Admin posts */}
+        {tab === 'posts' && <DashPosts />}
     </div>
   );
 }
