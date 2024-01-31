e484aaf9901f13105b2a8ec9e58ab30e875d4f8f
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-31 12:19:09 +0300

    completed the updatepost page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import FooterComponent from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 
 export default function App() {
@@ -26,6 +27,7 @@ export default function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute /> }>
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/ideas" element={<Projects />} />
       </Routes>
--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -1,21 +1,44 @@
 import { Button, Select, TextInput, FileInput, Alert } from "flowbite-react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 
-export default function CreatePost() {
+export default function UpdatePost() {
     const navigate = useNavigate();
     const [ file, setFile ] = useState(null);
     const [ imageUploadProgress, setImageUploadProgress ] = useState(null);
     const [ imageUploadError, setImageUploadError ] = useState(null);
     const [ formData, setFormData ] = useState({});
     const [ publishError, setPublishError ] = useState(null);
+    const { postId } = useParams();
+
+    useEffect(() => {
+        try {
+            const fetchPost = async () => {
+                const res = await fetch(`/api/post/getposts?postId=${postId}`);
+                const data = await res.json();
+                if (!res.ok) {
+                    console.log(error);
+                    setPublishError(data.message);
+                    return;
+                } 
+                if (res.ok) {
+                    setPublishError(null);
+                    setFormData(data.posts[0]);
+                }
+            };
+            fetchPost();
+        } catch (error) {
+            console.log(error)
+        }
+    }, [postId]);
+
     const handleUploadImage = async () => {
         try {
             if (!file) {
@@ -76,7 +99,7 @@ export default function CreatePost() {
     };
     return (
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
-            <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
+            <h1 className="text-center text-3xl my-7 font-semibold">Update post</h1>
             <form className="flex flex-col gap-4" onSubmit={handleSubmit} >
                 <div className="flex flex-col gap-4 sm:flex-row justify-between">
                     <TextInput 
@@ -85,9 +108,11 @@ export default function CreatePost() {
                         required 
                         id='title' 
                         className="flex-1" 
-                        onChange={(e)=> setFormData({...formData, title: e.target.value })} />
+                        onChange={(e)=> setFormData({...formData, title: e.target.value })}
+                        value={formData.title} />
                     <Select
                         onChange={(e)=> setFormData({ ...formData, category: e.target.value })}
+                        value={formData.category}
                     >
                         <option value="uncategorized">Select a category</option>
                         <option value="advertisement">Advertisement</option>
@@ -124,12 +149,13 @@ export default function CreatePost() {
                     theme="snow" 
                     placeholder="Share your thoughts..." 
                     className='h-72 mb-12' 
+                    value={formData.content}
                     required 
                     onChange={(value) => {
                     setFormData({ ...formData, content: value })}}
                     />
                 <Button type='submit' gradientDuoTone='purpleToPink' >
-                    Publish
+                    Update post
                 </Button>
                 {
                     publishError && <Alert color='failure' className="mt-5">
