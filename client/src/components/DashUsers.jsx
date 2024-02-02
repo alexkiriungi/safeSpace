90e8b09234f23a1b2ac4f4b6a1cf969f9fb0ca7e
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-02 23:33:26 +0300

    Creating the get users page

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -6,18 +6,18 @@ import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 export default function DashPosts() {
   const { currentUser } = useSelector(state => state.user);
-  const [ userPosts, setUserPosts ] = useState([]);
+  const [ users, setUsers ] = useState([]);
   const [ showMore, setShowMore ] = useState(true);
   const [ showModal, setShowModal ] = useState(false);
-  const [ postIdToDelete, setPostIdToDelete ] = useState('');
+  const [ userIdToDelete, setUserIdToDelete ] = useState('');
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchUsers = async () => {
       try {
-        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const res = await fetch(`/api/user/getusers`);
         const data = await res.json();
         if (res.ok) {
-          setUserPosts(data.posts);
-          if (data.posts.length < 9) {
+          setUsers(data.users);
+          if (data.users.length < 9) {
             setShowMore(false);
           }
         }
@@ -26,72 +26,57 @@ export default function DashPosts() {
       }
     };
     if (currentUser.isAdmin) {
-      fetchPosts();
+      fetchUsers();
     }
   }, [currentUser._id]);
 
   const handleShowMore = async () => {
-    const startIndex = userPosts.length;
+    const startIndex = users.length;
     try {
-      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const res = await fetch(`/api/user/getusers?startIndex=${startIndex}`);
       const data = await res.json();
       if (res.ok) {
-        setUserPosts((prev) => [...prev, ...data.posts]);
-        if (data.posts.length < 9) {
+        setUsers((prev) => [...prev, ...data.users]);
+        if (data.users.length < 9) {
           setShowMore(false);
         }
       }
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
     }
   };
 
-  const handleDeletePost = async () => {
-    setShowModal(false);
-    try {
-      const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`, {
-        method: 'DELETE'
-      });
-      const data = await res.json();
-      if (!res.ok) {
-        console.log(error);
-      } else {
-        setUserPosts((prev) => prev.filter((post) => post._id !== postIdToDelete));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   const handleDeleteUser = async () => {
+    
+//   };
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar 
       scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 
       dark:scrollbar-thumb-500'>
-      {currentUser.isAdmin && userPosts.length > 0 ? (
+      {currentUser.isAdmin && users.length > 0 ? (
         <>
           <Table hoverable className='shadow-md'>
             <Table.Head>
-              <Table.HeadCell>Date updated</Table.HeadCell>
-              <Table.HeadCell>Post image</Table.HeadCell>
-              <Table.HeadCell>Post title</Table.HeadCell>
-              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Date created</Table.HeadCell>
+              <Table.HeadCell>User image</Table.HeadCell>
+              <Table.HeadCell>Username</Table.HeadCell>
+              <Table.HeadCell>Admin</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
               <Table.HeadCell>
                 <span>Edit</span>
               </Table.HeadCell>
             </Table.Head>
-            { userPosts.map(post => (
+            { users.map((user) => (
               <Table.Body className='divide-y'>
                 <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                   <Table.Cell>
-                    { new Date(post.updatedAt).toLocaleDateString() }
+                    { new Date(user.createdAt).toLocaleDateString() }
                   </Table.Cell>
                   <Table.Cell>
-                    <Link to={`/post/${post.slug}`}>
-                      <img 
-                        src={post.image}
-                        alt={post.title} 
-                        className='w-20 h-10 object-cover bg-gray-500 rounded' />
-                    </Link>
+                    <img 
+                    src={user.profilePicture}
+                    alt={user.title} 
+                    className='w-20 h-10 object-cover bg-gray-500 rounded' />
                   </Table.Cell>
                   <Table.Cell>
                     <Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}>{post.title}</Link>
