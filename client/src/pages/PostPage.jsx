6e969632ffe54f62f8de4612106f58323848efc5
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-03 22:00:21 +0300

    PostPage skeleton created

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,13 +1,61 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
+import { Button, Spinner } from "flowbite-react";
 
 export default function PostPage() {
     const { postSlug } = useParams();
+    const [ loading, setLoading ] = useState(true);
+    const [ error, setError ] = useState(false);
+    const [ post, setPost ] = useState(null);
 
     useEffect(() => {
-
+        const fetchPost = async () => {
+            try {
+                setLoading(true);
+                const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
+                const data = await res.json();
+                if (!res.ok) {
+                    setError(true);
+                    setLoading(false);
+                    return
+                } 
+                if (res.ok) {
+                    setPost(data.posts[0]);
+                    setError(false);
+                    setLoading(false);
+                }
+            } catch (error) {
+                setError(true);
+                setLoading(false);
+            }
+        };
+        fetchPost();
     }, [postSlug]);
-  return (
-    <div>PostPage</div>
-  )
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center min-h-screen">
+                <Spinner size='xl' />
+            </div>
+        )
+    }
+    return <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+            <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto
+            lg:text-4xl'>
+                {post && post.title}
+            </h1>
+            <Link to={`/search?category=${post && post.category}`} 
+            className="self-center mt-5"
+            >
+                <Button color='gray' pill size='xs'>{post && post.category}</Button>
+            </Link>
+            <img src={post && post.image} alt={post && post.title} className="mt-10
+            p-3 max-h-[600] w-full object-cover" />
+            <div className="flex justify-between p-3 border-b border-slate-500
+            mx-auto w-full max-w-2xl text-xs">
+                <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+                <span>{post && (post.content.length /1000).toFixed(0)} mins read</span>
+            </div>
+        </main>;
+  
 }
