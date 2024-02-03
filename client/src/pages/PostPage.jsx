4daa2bd1872b25d73c7e5f1d39fd2ed3a9bd2877
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-03 22:27:55 +0300

    Complete post page functionality with custom stylings

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -54,7 +54,12 @@ export default function PostPage() {
             <div className="flex justify-between p-3 border-b border-slate-500
             mx-auto w-full max-w-2xl text-xs">
                 <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-                <span>{post && (post.content.length /1000).toFixed(0)} mins read</span>
+                <span className="italic">{post && (post.content.length /1000).toFixed(
+                    0)} mins read</span>
+            </div>
+            <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML=
+            {{__html: post && post.content}}>
+
             </div>
         </main>;
   
