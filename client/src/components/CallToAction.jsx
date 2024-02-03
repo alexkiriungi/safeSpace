3872a2d8d57d713676ca228d95ba57b443dbd81b
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-03 23:23:50 +0300

    completed the CallToAction section

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { Button } from "flowbite-react";
+
 
 export default function CallToAction() {
   return (
-    <div>CallToAction</div>
-  )
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500
+    items-center rounded-tl-3xl rounded-br-3xl text-center">
+        <div className="flex-1 justify-center flex flex-col">
+            <h2 className="text-2xl">
+                Do you feel like a weirdo?
+            </h2>
+            <p className="text-gray-500 my-2">
+                Welcome to our social platform for weirdos!
+            </p>
+            <Button gradientDuoTone='purpleToPink' className="rounded-tl-xl
+            rounded-bl-none">
+                <a href="https://chimerical-cupcake-e49e36.netlify.app" target="_blank"
+                rel='noopener noreferrer'>Shadapedia</a>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+            <img src="https://www.shutterstock.com/image-vector/hand-drawn-vector-
+            doodles-set-600nw-1425794720.jpg" />
+        </div>
+    </div>
+  );
 }
