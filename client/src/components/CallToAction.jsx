d555aba9c6cff8a43a0046206c6be375400933a4
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-03 22:33:29 +0300

    Added the CallToAction component skeleton

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CallToAction() {
+  return (
+    <div>CallToAction</div>
+  )
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
 import { Button, Spinner } from "flowbite-react";
+import CallToAction from "../components/CallToAction";
 
 export default function PostPage() {
     const { postSlug } = useParams();
@@ -58,8 +59,9 @@ export default function PostPage() {
                     0)} mins read</span>
             </div>
             <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML=
-            {{__html: post && post.content}}>
-
+            {{__html: post && post.content}}></div>
+            <div className="max-w-4xl mx-auto w-full">
+                <CallToAction />
             </div>
         </main>;
   
