849561721da358d7678c53cbfb754679fc9b93ee
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-04 07:47:41 +0300

    Added the scoll to top functionality

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,5 +1,28 @@
-export default function CommentSection () {
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+export default function CommentSection ({postId}) {
+    const currentUser = useSelector(state => state.user);
     return (
-        <div>CommentSection</div>
-    )
+        <div className='max-w-2xl mx-auto w-full p-3'>
+            {currentUser ? 
+            (
+                <div className='flex items-center gap-1 my-5 text-gray-500
+                text-sm'> 
+                    <p>Signed in as: </p>
+                    <img className='h-5 w-5 object-cover rounded-full' src=
+                        {currentUser.profilePicture} alt={currentUser.username} />
+                    <Link to={'/dashboard?tab=profile'} className='text-xs 
+                    text-cyan-600 hover-underline '>
+                        @{currentUser.username}
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    You must be signed in to comment.
+                    <Link to={'/sign-in'}></Link>
+                </div>
+            )}
+        </div>
+    );
 }
