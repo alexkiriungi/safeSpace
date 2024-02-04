73e67bb44a9576c324f54cb068c7bde57fd4a4c3
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-04 09:01:56 +0300

    created the post comment api route

---
+++ api/controllers/comment.controller.js
@@ -0,0 +1,23 @@
+import Comment from "../models/comment.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const createComment = async (req, res, next) => {
+    try {
+        const { content, postId, userId } = req.body;
+
+        if (userId !== req.user.id) {
+            return next(errorHandler(403, 'You are not allowed to create this comment'));
+        }
+
+        const newComment = new Comment({
+            content,
+            postId,
+            userId
+        });
+        await newComment.save();
+
+        res.status(201).json(newComment);
+    } catch (error) {
+        next(error);
+    }
+};
