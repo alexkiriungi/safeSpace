cddb2e41c7c30cb3c503b81189e66cf2100d7935
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-04 08:04:31 +0300

    comment section UI

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,8 +1,11 @@
+import { Button, TextInput, Textarea } from 'flowbite-react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default function CommentSection ({postId}) {
     const currentUser = useSelector(state => state.user);
+    const [ comment, setComment ] = useState('');
     return (
         <div className='max-w-2xl mx-auto w-full p-3'>
             {currentUser ? 
@@ -10,18 +13,39 @@ export default function CommentSection ({postId}) {
                 <div className='flex items-center gap-1 my-5 text-gray-500
                 text-sm'> 
                     <p>Signed in as: </p>
-                    <img className='h-5 w-5 object-cover rounded-full' src=
-                        {currentUser.profilePicture} alt={currentUser.username} />
+                    <img className='h-5 w-5 object-cover rounded-full' 
+                    src={currentUser.profilePicture} alt={currentUser.username} />
                     <Link to={'/dashboard?tab=profile'} className='text-xs 
                     text-cyan-600 hover-underline '>
                         @{currentUser.username}
                     </Link>
                 </div>
             ) : (
-                <div>
+                <div className='text-sm text-teal-500 my-5 flex gap-1'>
                     You must be signed in to comment.
-                    <Link to={'/sign-in'}></Link>
+                    <Link to={'/sign-in'} className='text-blue-500
+                    hover-underline'>Sign in</Link>
                 </div>
+            )}
+            {currentUser && (
+                <form className='border border-teal-500 rounded-md
+                p-3'>
+                    <Textarea 
+                    placeholder='Add a comment...'
+                    rows='3'
+                    maxLength='200' 
+                    onChange={(e)=> setComment(e.target.value)}
+                    value={comment} />
+                    <div className='flex justify-between items-center mt-5'>
+                        <p className='text-gray-500 text-xs'>
+                            200 characters left
+                        </p>
+                        <Button outline gradientDuoTone='purpleToBlue' 
+                        type='submit'>
+                            Submit
+                        </Button>
+                    </div>
+                </form>
             )}
         </div>
     );
