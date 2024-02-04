f6f615b0d5bb5880eba9c7ea374a2c8d22470eed
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-04 07:25:03 +0300

    Adding the Comment section UI

---
+++ client/src/components/CommentSection.jsx
@@ -0,0 +1,5 @@
+export default function CommentSection () {
+    return (
+        <div>CommentSection</div>
+    )
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
 import { Button, Spinner } from "flowbite-react";
 import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 export default function PostPage() {
     const { postSlug } = useParams();
@@ -63,6 +64,7 @@ export default function PostPage() {
             <div className="max-w-4xl mx-auto w-full">
                 <CallToAction />
             </div>
+            <CommentSection postId={post._id} />
         </main>;
   
 }
