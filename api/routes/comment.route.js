1d6042540648d2b0488d7b5b8c7f09c5cc6b4654
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-05 13:34:23 +0300

    Edit comment API  route complete

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, getPostComments, likeComment } from '../controllers/comment.controller.js';
+import { createComment, getPostComments, likeComment, editComment } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
 
 export default router;
