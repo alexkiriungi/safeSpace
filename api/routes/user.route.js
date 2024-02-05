3b524c28c4e255b453858bc342934f441db6e8e7
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-05 11:32:50 +0300

    created the get user API route in usersmodel

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import { test, updateUser, deleteUser, signout, getUsers } from "../controllers/user.controller.js";
+import { test, updateUser, deleteUser, signout, getUsers, getUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
+router.get(':userId', getUser);
 
 export default router;
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useEffect } from "react"
 
-export default function Comment() {
-  return (
-    <div>Comment</div>
+
+export default function Comment({comment}) {
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                
+            } catch (error) {
+                console.log(error.message)
+            }
+        };
+        getUser();
+    }, [comment]);
+    return (
+        <div>Comment</div>
   )
 }
