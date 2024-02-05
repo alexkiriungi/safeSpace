16a15cc3bbfc01e6c2f42dfe31fa8f22e55de0df
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-05 14:11:22 +0300

    Edit comment functionality complete

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
 
 
-export default function Comment({comment, onLike }) {
+export default function Comment({comment, onLike, onEdit }) {
     const { currentUser } = useSelector((state) => state.user);
     const [ isEditing, setIsEditing ] = useState(false);
     const [ user, setUser ] = useState({});
@@ -28,6 +28,27 @@ export default function Comment({comment, onLike }) {
         setIsEditing(true);
         setEditedContent(comment.content);
     };
+
+    const handleSave = async () => {
+        try {
+            const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    content: editedContent
+                })
+            });
+            if (res.ok) {
+                setIsEditing(false);
+                onEdit(comment, editedContent);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
             <div className="flex-shrink-0 mr-3">
@@ -55,6 +76,7 @@ export default function Comment({comment, onLike }) {
                             type="button"
                             size='sm'
                             gradientDuoTone='purpleToBlue'
+                            onClick={handleSave}
                             >
                                 Save
                             </Button>
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -61,20 +61,25 @@ export default function CommentSection ({postId}) {
             });
             if (res.ok) {
                 const data = await res.json();
-                setComments(comments.map((comment) => {
+                setComments(comments.map((comment) => 
                     comment._id === commentId ? {
                         ...comment,
                         likes: data.likes,
                         numberOfLikes: data.likes.length,
                     } : comment
-                }));
+                ));
             }
         } catch (error) {
             console.log(error.message);
         }
     };
 
-    
+    const handleEdit = async (comment, editedContent) => {
+        setComments(
+            comments.map((c) => 
+            c._id === comment.id ? { ...c, content: editedContent } : c)
+        );
+    };
 
     return (
         <div className='max-w-2xl mx-auto w-full p-3'>
@@ -134,9 +139,10 @@ export default function CommentSection ({postId}) {
                     </div>
                     {comments.map((comment) => (
                         <Comment 
-                        key={comment?._id}
+                        key={comment._id}
                         comment={comment} 
-                        onLike={handleLike} />
+                        onLike={handleLike}
+                        onEdit={handleEdit} />
                     ))}
                 </>
             )}
