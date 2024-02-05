2afe204855e6852b157d40a72f6c7367b5770a85
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-05 12:29:08 +0300

    Added the Likecomment api route

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, getPostComments } from '../controllers/comment.controller.js';
+import { createComment, getPostComments, likeComment } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 
 export default router;
