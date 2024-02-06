73b2035abf9bee86fcbf81a1a17f9655c802b7cb
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-06 09:11:39 +0300

    delete comment API complete

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, getPostComments, likeComment, editComment } from '../controllers/comment.controller.js';
+import { createComment, getPostComments, likeComment, editComment, deleteComment } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -9,5 +9,6 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.put('/deleteComment/:commentId', verifyToken, deleteComment);
 
 export default router;
