c09f9c2e87e679a0a4f0dbf117ab7273bfa7717c
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-07 12:41:39 +0300

    minor modify fix

--- client/src/components/DashboardComp.jsx
+++ client/src/components/DashboardComp.jsx
@@ -79,15 +79,15 @@ export default function DashboardComp() {
                 <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
                     <div className="flex justify-between">
                         <div className="">
-                            <h3 className='text-gray-500 text-md uppercase'>Total Users</h3>
-                            <p className='text-2xl'>{totalUsers}</p>
+                            <h3 className='text-gray-500 text-md uppercase'>Total Comments</h3>
+                            <p className='text-2xl'>{totalComments}</p>
                         </div>
-                        <HiOutlineUserGroup className='bg-teal-600 text-white rounded-full text-5xl p-3 shadow-lg'/>
+                        <HiAnnotation className='bg-teal-600 text-white rounded-full text-5xl p-3 shadow-lg'/>
                     </div>
                     <div className="flex gap-2 text-sm">
                         <span className='text-green-500 flex items-center'>
                             <HiArrowNarrowUp />
-                            {lastMonthUsers}
+                            {lastMonthComments}
                         </span>
                         <div className="text-gray-500">Last month</div>
                     </div>
