243b67886d3c09b699a632326387883fd1bc8556
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-07 18:18:04 +0300

    Added and completed search page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
-import Search from './components/Search';
+import Search from './pages/Search';
 
 
 export default function App() {
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
-import { TextInput } from "flowbite-react";
+import { Button, Select, TextInput } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import PostCard from "../components/PostCard";
 
 export default function Search() {
     const location = useLocation();
@@ -12,6 +13,7 @@ export default function Search() {
     const [ posts, setPosts ] = useState([]);
     const [ loading, setLoading ] = useState(false);
     const [ showMore, setShowMore ] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(()=>{
         const urlParams = new URLSearchParams(location.search);
@@ -41,7 +43,9 @@ export default function Search() {
                 setPosts(data.posts);
                 if (data.posts.length === 9) {
                     setShowMore(true);
-                } else {false}
+                } else {
+                    setShowMore(false);
+                }
             }
         };
         fetchPosts();
@@ -56,15 +60,45 @@ export default function Search() {
             setSidebarData({ ...sidebarData, sort: order });
         }
         if (e.target.value) {
-            const category = e.targey.value || 'uncategorized';
+            const category = e.target.value || 'uncategorized';
             setSidebarData({ ...sidebarData, category });
         }
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', sidebarData.searchTerm);
+        urlParams.set('sort', sidebarData.sort);
+        urlParams.set('category', sidebarData.category);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    const handleShowMore = async () => {
+        const numberOfPosts = posts.length;
+        const startIndex = numberOfPosts;
+        const urlParams = URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/post/getposts?${searchQuery}`);
+        if (!res.ok) {
+            return;
+        }
+        if (res.ok) {
+            const data = await res.json();
+            setPosts([...posts, ...data.posts]);
+            if (data.posts.length === 9) {
+                setShowMore(true);
+            } else {
+                setShowMore(false);
+            }
+        }
+    };
     return (
         <div className="flex flex-col md:flex-row">
             <div className="p-7 border-b md:border-r md:min-h-screen border-gray-500">
-                <form className="flex flex-col gap-8">
+                <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
                     <div className="flex items-center gap-2">
                         <label className="whitespace-nowrap font-semibold">
                             Search Term:
@@ -74,10 +108,59 @@ export default function Search() {
                         value={sidebarData.searchTerm}
                         onChange={handleChange}/>
                     </div>
-                    <div className="">
+                    <div className="flex items-center gap-2">
                         <label className="font-semibold ">Sort:</label>
+                        <Select onChange={handleChange} value={sidebarData.sort}
+                        id="sort"
+                        >
+                            <option value='desc'>Latest</option>
+                            <option value='asc' >Oldest</option>
+                        </Select>
                     </div>
+                    <div className="flex items-center gap-2">
+                        <label className="font-semibold ">Category:</label>
+                        <Select onChange={handleChange} value={sidebarData.category}
+                        id="category"
+                        >
+                            <option value='uncategorized'>Uncategorized</option>
+                            <option value='advertisement' >Advertisement</option>
+                            <option value='technology'>Technology</option>
+                            <option value='inspiration' >Inpiration</option>
+                            <option value='regrets' >Regrets</option>
+                            <option value='meme' >Meme</option>
+                        </Select>
+                    </div>
+                    <Button type='submit' outline gradientDuoTone='purpleToPink'>
+                        Apply Filters
+                    </Button>
                 </form>
+            </div>
+            <div className="w-full">
+                <h1 className="text-3xl font-semibold sm:border-b border-gray-500
+                p-3 mt-5">
+                    Search Results
+                </h1>
+                <div className="p-7 flex flex-wrap gap-4">
+                    {
+                        !loading && posts.length === 0 && (<p className='text-xl text-gray-500'>
+                            No posts found!
+                        </p>
+                    )}
+                    {
+                        loading && <p className='text-xl text-gray-500'>Loading...</p>
+                    }
+                    {
+                        !loading && posts.map((post) => <PostCard key={post._id} post={post} />
+                        )
+                    }
+                    {
+                        showMore && <button onClick={handleShowMore} 
+                        className="text-teal-500 text-lg
+                        hover:underline p-7 w-full">
+                            Show More
+                        </button>
+                    }
+                </div>
             </div>
         </div>
     );
