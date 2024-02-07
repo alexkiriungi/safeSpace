7c6ed8aea7773e2ff2596b05b9ec0a484282ac41
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-07 19:15:26 +0300

    minor changes

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 
 dotenv.config();
@@ -25,6 +26,8 @@ mongoose.connect(process.env.MONGO_URL).then(
     console.log(error);
 });
 
+const __dirname = path.resolve();
+
 app.listen(port, ()=> {
     console.log(`Server is running on ${port}`);
 });
@@ -33,6 +36,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
