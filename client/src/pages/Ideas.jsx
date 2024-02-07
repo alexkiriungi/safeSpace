028326f3ac2ff1bdfa4b26e1466aaab40c02c82e
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-07 18:58:41 +0300

    complete the ideas page

--- client/src/pages/Ideas.jsx
+++ client/src/pages/Ideas.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction';
 
 export default function Ideas() {
   return (
-    <div>Ideas</div>
-  )
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center
+    items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Ideas</h1>
+      <p className='text-lg text-gray-500'>
+        A share a day keeps the vices at bay...
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
