1f6e104e0f1805a74d9771e4e02758a6bcd077eb
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-07 13:24:15 +0300

    Recent users table complete

--- client/src/components/DashboardComp.jsx
+++ client/src/components/DashboardComp.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { HiArrowNarrowUp, HiOutlineUserGroup, HiAnnotation, HiDocumentText } from 'react-icons/hi';
+import { Button, Table, TableCell } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 export default function DashboardComp() {
     const [ users, setUsers ] = useState([]);
@@ -107,6 +109,40 @@ export default function DashboardComp() {
                         </span>
                         <div className="text-gray-500">Last month</div>
                     </div>
+                </div>
+            </div>
+            <div className=''>
+                <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800'>
+                    <div className='flex justify-between p-3 text-sm font-semibold'>
+                        <h1 className='text-center p-2'>Recent users</h1>
+                        <Button outline gradientDuoTone='purpleToPink'>
+                            <Link to={'/dashboard?tab=users'} >See all</Link>
+                        </Button>
+                    </div>
+                    <Table hoverable>
+                        <Table.Head>
+                            <Table.HeadCell>User Image</Table.HeadCell>
+                            <Table.HeadCell>Username</Table.HeadCell>
+                        </Table.Head>
+                        {users && users.map((user) => (
+                            <Table.Body key={user._id} className='divide-y'>
+                                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                                    <Table.Cell>
+                                        <img 
+                                        src={user.profilePicture}
+                                        alt='user'
+                                        className='w-10 h-10 rounded-full bg-gray-500'
+                                        />
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link to={`/profile/${user.username}`}>
+                                            {user.username}
+                                        </Link>
+                                    </Table.Cell>
+                                </Table.Row>
+                            </Table.Body>
+                        ))}
+                    </Table>
                 </div>
             </div>
         </div>
