c807cac6b9a088a2b746d875a82c91b364b8f819
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-07 09:53:25 +0300

    minor modify to DashComponenst

--- client/src/components/DashboardComp.jsx
+++ client/src/components/DashboardComp.jsx
@@ -1,4 +1,16 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
 export default function DashboardComp() {
+    const [ users, setUsers ] = useState([]);
+    const [ comments, setComments ] = useState([]);
+    const [ posts, setPosts ] = useState([]);
+    const [ totalUsers, setTotalUsers ] = useState(0);
+    const [ totalPosts, setTotalPosts ] = useState(0);
+    const [ totalComments, setTotalComments ] = useState(0);
+    const [ lasMonthUsers, setLastMonthUsers ] = useState(0);
+    const [ lastMonthPosts, setLastMonthPosts ] = usesState(0);
+    const [ lastMonthComments, setLastMonthComments ] = useState(0);
     return (
         <div>DashboardComp</div>
     );
